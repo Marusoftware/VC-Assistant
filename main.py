44c91e46024a444f3633f56a78863bbe073b82b0
Marusoftware <marusoftware@outlook.jp>
2021-12-03 20:42:26 +0900

    support Voice Activity

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from discord import SelectOption, Option, Member, Embed
+from discord import SelectOption, Option, Member, Embed, EmbeddedActivity
 import logging, argparse, discord, random, string, re, datetime, os, io, traceback
 from pytube.request import stream
 
@@ -876,6 +876,20 @@ async def mvt(ctx, channel:discord.VoiceChannel):
 @bot.slash_command(name="move2", desecription="Move to Another VC")
 async def mvt_sl(ctx, channel:Option(discord.VoiceChannel, "VC", required=True)):
     await mvt(ctx, channel)
+#activity
+@bot.command(name="activity", aliases=["act"], desecription="VC Activity")
+async def act(ctx, activity:str):
+    activity=getattr(EmbeddedActivity, activity, None)
+    if activity is None:
+        await Send(ctx, "No such activity was found.")
+    else:
+        invite=await ctx.guild.voice_client.channel.create_activity_invite(activity, temporary=True)
+        await Send(ctx, f'Click here to join VC Activity. \n {invite.url}')
+@bot.slash_command(name="activity", desecription="VC Activity")
+async def act_sl(ctx, activity:Option(str, "Activity Type", required=True, choices=["awkword",
+"betrayal","checkers_in_the_park","chess_in_the_park","doodle_crew","fishington","letter_tile","ocho",
+"poker_night","putts","sketchy_artist","spell_cast","watch_together","word_snacks","youtube_together"])):
+    await act(ctx, activity)
 ##tts
 if enjtalk:
     @bot.group(name="tts", desecription="Text to Speech!!")
